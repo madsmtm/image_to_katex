a31aa88ed43c1ebb86385d891c4d10a9e4c7f3c5
Mads Marquart <madsmtm@gmail.com>
2018-09-29 22:59:45 +0200

    Added basic setup, and full `image_to_katex` functionality

---
+++ katex/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: UTF-8 -*-
+from __future__ import unicode_literals
+
+from .core import *
+from .models import *
---
+++ setup.py
@@ -0,0 +1,45 @@
+# -*- coding: UTF-8 -*-
+from __future__ import unicode_literals
+
+from setuptools import setup
+
+setup(
+    name="katex",
+    version="attr: katex.__version__",
+    license="BSD 3-Clause",
+    license_file="LICENSE",
+    author="Mads Marquart",
+    author_email="madsmtm@gmail.com",
+    description="Convert an image into KaTeX to use in Facebook Messenger",
+    long_description="file: readme.md",
+    long_description_content_type="text/markdown",
+    keywords="Facebook KaTeX Messenger ",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Communications :: Chat",
+        "Topic :: Games/Entertainment",
+        "Topic :: Scientific/Engineering :: Human Machine Interfaces",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+    ],
+    url="http://github.com/madsmtm/katex",
+    zip_safe=True,
+    packages=["katex"],
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4.0",
+    install_requires=["Pillow>=5.2.0", "attrs", "parse", "Click"],
+    entry_points={"console_scripts": ["image_to_katex=katex.cli:image_to_katex"]},
+)
